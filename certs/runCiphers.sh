e857fbf2efd2450ce5eda233f381473293af6114
Kat <khines2@uccs.edu>
2022-10-20 12:37:36 -0700

    added timestamps to beginning and end of code

--- certs/runCiphers.sh
+++ certs/runCiphers.sh
@@ -2,12 +2,15 @@ rm -rf Signatures.o
 
 # -g for debugging
 g++ -g -o Signatures.o Signatures.cpp -L../build/lib -loqs
+g++ -o runRSA runRSA.cpp
 
 LD_LIBRARY_PATH=/usr/local/lib
 export LD_LIBRARY_PATH
 
+date
+
 ./Signatures.o 
 
-g++ -o runRSA runRSA.cpp
-
 ./runRSA
+
+date
